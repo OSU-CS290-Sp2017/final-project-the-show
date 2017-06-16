b9f32816f9a650b5b10fb690859484654bfa9974
Donghao Lin <lindo@oregonstate.edu>
2017-06-16 10:40:09 -0700

    change index.js

---
+++ public/adminTemplate.js
@@ -0,0 +1,27 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['adminCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<tr>\n  <td>"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "</td>\n  <td>"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</td>\n  <td>$"
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "</td>\n  <td>"
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "</td>\n  <td><button class=\"btn btn-danger\" id='remove-button' onclick=\"deleteRow(this)\">Remove</button></td>\n</tr>\n";
+},"useData":true});
+templates['menuCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"card\">\n  <img src=\""
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\" alt=\"Card image cap\">\n  <p class=\"card-text\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</p>\n  <div class=\"text-right\">\n    <button type=\"button\" class=\"btn btn-outline-secondary\">$"
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "</button>\n  </div>\n</div>\n";
+},"useData":true});
+})();
--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,79 @@
  * Header js control
  */
  console.log("index.js loaded");
+
+ var allmenu = [];
+ function generateNewmenuElem(Numbers, names ,Prices, PicURL ) {
+
+    var menuTemplate = Handlebars.templates.adminCard;
+    var menuData = {
+      id: Numbers,
+      name: names,
+      price: Prices,
+      url: PicURL
+    };
+    return menuTemplate(menuData);
+  }
+
+  function insertNewmenu(){
+    var numberinput = document.getElementById('number-input').value;
+    var nameinput = document.getElementById('name-input').value;
+    var priceinput = document.getElementById('price-input').value;
+    var picinput = document.getElementById('picurl-input').value;
+
+    var newmenu = generateNewmenuElem(numberinput, nameinput, priceinput, picinput);
+    var adminContainer = document.getElementById('admin-card');
+    adminContainer.insertAdjacentHTML('afterbegin',newmenu);
+
+    var menuContainer = document.getElementById('menu-card');
+    menuContainer.insertAdjacentHTML('afterbegin',newmenu);
+    clearTwitInputValues();
+  }
+
+  function clearTwitInputValues() {
+
+    var textInputElems = document.getElementsByClassName('text-input-element');
+    for (var i = 0; i < textInputElems.length; i++) {
+      var input = textInputElems[i].querySelector('input, textarea');
+      input.value = '';
+    }
+
+  }
+
+  function deleteRow(rowid)
+  {
+      var row = document.getElementById('adminCard');
+      row.parentNode.removeChild(row);
+  }
+/*
+ window.addEventListener('DOMContentLoaded', function () {
+   var addbutton = document.getElementById('add-button');
+   addbutton.addEventListener('click',insertNewmenu);
+*/
+
+function insertNewDishes () {
+  var numberInput = document.getElementById('number-input').value;
+  var nameInput = document.getElementById('name-input').value;
+  var priceInput = document.getElementById('price-input').value;
+  var picInput = document.getElementById('picurl-input').value;
+
+  var obj = {
+    id: numberInput,
+    name: nameInput,
+    price: priceInput,
+    url: picInput
+  };
+
+  tempStorage = JSON.stringify({dishes:[obj]});
+  Handlebars.registerHelper('json', function (context) {
+    return tempStorage;
+  });
+  console.log(JSON.stringify({dishes:[obj]}));
+}
+
+
+var addbutton = document.getElementById('add-button');
+  addbutton.addEventListener('click',insertNewmenu);
+
+var removebutton = document.getElementById('remove-button');
+  removebutton.addEventListener('click',deleteRow);
