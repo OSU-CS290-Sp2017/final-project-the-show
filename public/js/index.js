838968442c0c35af165dec0146cb6ca2613948f3
刘启邦 <Austin@liuqibangdeMacBook-Air.local>
2017-06-16 01:50:23 -0700

    change

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,18 @@
  * Header js control
  */
  console.log("index.js loaded");
+
+var name;
+var price;
+
+$('.btn-outline-secondary').click(function(){
+  name = $(this).attr("name");
+  price = $(this).attr("price");
+  console.log(name);
+  console.log(price);
+  alert('dish added to check out cart');
+});
+
+function insertinfon (){
+  document.getElementById('product-name').innerHTML = name;
+}
